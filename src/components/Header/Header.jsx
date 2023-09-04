ce4d208d13d5b91a4f59702c45f67fbe7526bafc
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-09-04 20:31:22 +0100

    make it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react';
 import './Header.css';
+import { BiMenuAltRight } from 'react-icons/bi';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const Header = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const getMenuStyles = () => {
+    if (document.documentElement.clientWidth <= 800) {
+      return {
+        right: !toggleMenu && '-100%',
+      };
+    }
+  };
   return (
     <section className="h-wrapper">
       <div className="h-container flexCenter innerWidth paddings">
         <img src="./logo.png" alt="logo" width={100} />
-
-        <div className="flexCenter h-menu">
-          <a href="">Residencies</a>
-          <a href="">Our Values</a>
-          <a href="">Contact</a>
-          <a href="">Get Started</a>
-          <button className="button">
+        <OutsideClickHandler onOutsideClick={() => setToggleMenu(false)}>
+          <div className="flexCenter h-menu" style={getMenuStyles()}>
+            <a href="">Residencies</a>
+            <a href="">Our Values</a>
             <a href="">Contact</a>
-          </button>
+            <a href="">Get Started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+
+        <div className="menu-icon" onClick={() => setToggleMenu(!toggleMenu)}>
+          <BiMenuAltRight size={30} />
         </div>
       </div>
     </section>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import { HiLocationMarker } from 'react-icons/hi';
 import Countup from 'react-countup';
+import { motion } from 'framer-motion';
 import './Hero.css';
 const Hero = () => {
   return (
@@ -9,9 +10,16 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle"></div>
-            <h1>
+            <motion.h1
+              initial={{ y: '2rem', opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: 'spring',
+              }}
+            >
               Discover <br /> Most Suitable <br /> Property
-            </h1>
+            </motion.h1>
           </div>
           <div className="flexColStart her-des">
             <span className="secondaryText">
@@ -54,9 +62,17 @@ const Hero = () => {
 
         {/* right side */}
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div
+            className="image-container"
+            initial={{ x: '7rem', opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: 'spring',
+            }}
+          >
             <img src="./hero-image.png" alt="hero" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
