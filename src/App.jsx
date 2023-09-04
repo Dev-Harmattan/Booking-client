4495885da9ec6d0890bb2067e4cd15c0ab2130b9
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-09-04 17:28:06 +0100

    contact view

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero/Hero';
 import './App.css';
 import { Residencies } from './components/Residencies/Residencies';
 import { Value } from './components/Value/Value';
+import { Contact } from './components/Contact/Contact';
+import { GetStarted } from './components/GetStarted/GetStarted';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
       <Companies />
       <Residencies />
       <Value />
+      <Contact />
+      <GetStarted />
     </div>
   );
 }
