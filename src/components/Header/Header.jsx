bbfaee3edd19299aa0624061672bf210bb415a37
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-09-08 09:18:55 +0100

    make it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,12 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { MenuProfile } from '../MenuProfile/MenuProfile';
 import { useAuthCheck } from '../../hooks/useAuthCheck';
 import { AddPropertyModal } from '../AddPropertyModal/AddPropertyModal';
+import { getMenuStyles } from '../../utils/common';
+import { useHeaderColor } from '../../hooks/useHeaderColor';
 
 const Header = () => {
-  const [toggleMenu, setToggleMenu] = useState(false);
+  const headerColor = useHeaderColor();
+  const [menuOpened, setMenuOpened] = useState(false);
   const [modalOpened, setModalOpened] = useState(false);
   const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
   const { validateLogin } = useAuthCheck();
@@ -20,27 +23,33 @@ const Header = () => {
     }
   };
 
-  const getMenuStyles = () => {
-    if (document.documentElement.clientWidth <= 800) {
-      return {
-        right: !toggleMenu && '-100%',
-      };
-    }
-  };
   return (
-    <section className="h-wrapper">
-      <div className="h-container flexCenter innerWidth paddings">
+    <section className="h-wrapper" style={{ background: headerColor }}>
+      <div className="flexCenter innerWidth paddings h-container">
+        {/* logo */}
         <Link to="/">
           <img src="./logo.png" alt="logo" width={100} />
         </Link>
-        <OutsideClickHandler onOutsideClick={() => setToggleMenu(false)}>
-          <div className="flexCenter h-menu" style={getMenuStyles()}>
-            <NavLink to="/properties">Properties</NavLink>
-            <a href="mailto:ganiyusodiq@gmail.com">Contact</a>
 
+        {/* menu */}
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setMenuOpened(false);
+          }}
+        >
+          <div
+            // ref={menuRef}
+            className="flexCenter h-menu"
+            style={getMenuStyles(menuOpened)}
+          >
+            <NavLink to="/properties">Properties</NavLink>
+
+            <a href="mailto:zainkeepscode@gmail.com">Contact</a>
+
+            {/* add property */}
             <div onClick={handleAddPropertyClick}>Add Property</div>
             <AddPropertyModal opened={modalOpened} setOpened={setModalOpened} />
-
+            {/* login button */}
             {!isAuthenticated ? (
               <button className="button" onClick={loginWithRedirect}>
                 Login
@@ -51,9 +60,10 @@ const Header = () => {
           </div>
         </OutsideClickHandler>
 
+        {/* for medium and small screens */}
         <div
           className="menu-icon"
-          onClick={() => setToggleMenu((prev) => !prev)}
+          onClick={() => setMenuOpened((prev) => !prev)}
         >
           <BiMenuAltRight size={30} />
         </div>
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,9 @@
+export const getMenuStyles = (menuOpened) => {
+  if (document.documentElement.clientWidth <= 800) {
+    return { right: !menuOpened && '-100%' };
+  }
+};
+
 export const sliderSettings = {
   slidesPerView: 1,
   spaceBetween: 50,
